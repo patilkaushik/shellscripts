2b19b0c45ed2bf722c83014890e25c5f4bc81c6d
Kaushik Patil <41260894+patilkaushik@users.noreply.github.com>
2019-10-26 18:05:02 +0530

    Create EMonF.sh

---
+++ EMonF.sh
@@ -0,0 +1,30 @@
+#/bin/bash
+
+######################################
+# This is Email Monitoring Framework #
+# This provides template for writing #
+# Email monitoring scripts           #
+######################################
+# Author:                            #
+# Contact:                           #
+# Description:                       #
+# Version:                           #
+######################################
+
+# Definition
+
+MAILLIST=name@email.com,yourname@email.com
+EMAILBODY="This is your email alert"
+LOGDIR="/path/to/logdir"
+TMPDIR="/path/to/tmpdir"
+
+# Your logic goes here
+
+#if [ true ]
+#then
+#fi
+
+
+# Send email
+
+echo "$EMAILBODY"|mail -s "Alert subject goes here" -r "Name<name@mail.com>" -a /path/to/attachment $MAILLIST
