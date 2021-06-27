b78c58a3802275162c0b146925caf97c3624e7b2
Kaushik Patil <41260894+patilkaushik@users.noreply.github.com>
2021-06-27 16:34:33 +0530

    Update sigtrap.sh

--- sigtrap.sh
+++ sigtrap.sh
@@ -12,7 +12,7 @@ LOG=/tmp/`basename $0`.log
 
 # Gather a list of the signals, then trap them
 # I don't know why I included SIGKILL -9 ;)
-for sig in `kill -l | sed -r 's/[0-9]{1,2}\)//g'`
+for sig in $(kill -l | sed -r 's/[0-9]{1,2}\)//g')
 do
 	trap "dothis $sig" $sig
 done
@@ -20,9 +20,9 @@ done
 # Print intercepted kill signal and log it
 function dothis()
 {
-	cmd="echo "`trap -l|sed 's/)//g'`"|sed -n 's/$1.*//p'|awk '{print \$NF}'"
+	cmd="echo "$(trap -l|sed 's/)//g')"|sed -n 's/$1.*//p'|awk '{print \$NF}'"
 	SIGNUM=`eval $cmd`
-	printf "`date +"%Y-%m-%d %T"` - Intercepted signal $SIGNUM $1"| tee -a $LOG
+	printf "$(date +"%Y-%m-%d %T") - Intercepted signal $SIGNUM $1"| tee -a $LOG
 	exit $SIGNUM
 }
 
